cdd1654a8e2677af7e5fae1045ac546f5652ccf0
Samson Okunola <samson.okunola@technologies.com>
2026-05-29 16:42:03 +0100

    chore: exposed the types and libs

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from "./constants/index.js";
 export * from "./hooks/index.js";
 export * from "./icons/index.js";
 export * from "./lib/index.js";
+export * from "./themes/index.js";
+export * from "./types/index.js";
